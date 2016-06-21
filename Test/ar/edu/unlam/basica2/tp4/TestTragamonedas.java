a38ebaddc2bd30e4f94bc8557f746ac249d5df19
sergio <sergio.f.balanda@gmail.com>
2016-06-21 17:30:29 -0300

    Cambios en el test.

--- Test/ar/edu/unlam/basica2/tp4/TestTragamonedas.java
+++ Test/ar/edu/unlam/basica2/tp4/TestTragamonedas.java
@@ -10,24 +10,43 @@ public class TestTragamonedas {
 	public void testTragamonedas() {
 		Tragamonedas miTragamonedas = new Tragamonedas();
 		miTragamonedas.activar();
-		System.out.println("Posiciones");
-		System.out.println("Posicion tambor uno: "+miTragamonedas.tamborUno.getPosicion());
-		System.out.println("Posicion tambor uno: "+miTragamonedas.tamborDos.getPosicion());
-		System.out.println("Posicion tambor uno: "+miTragamonedas.tamborTres.getPosicion());
+		Integer posicionTamborUno = miTragamonedas.tamborUno.getPosicion();
+		Integer posicionTamborDos = miTragamonedas.tamborDos.getPosicion();
+		Integer posicionTamborTres = miTragamonedas.tamborTres.getPosicion();
+		assertTrue( 
+				(posicionTamborUno >= 1 && posicionTamborUno <= 10) &&
+				(posicionTamborDos >= 1 && posicionTamborDos <= 10) &&
+				(posicionTamborTres >= 1 && posicionTamborTres <= 10)
+				);
 	}
+	
+	@Test
+	public void comprobarQueLosTamboresEstenEnUnoAntesDeActivar(){
+		Tragamonedas miTragamonedas = new Tragamonedas();
+		Integer valorEsperado = 1;
+		assertEquals(valorEsperado, miTragamonedas.tamborUno.getPosicion());
+		assertEquals(valorEsperado, miTragamonedas.tamborDos.getPosicion());
+		assertEquals(valorEsperado, miTragamonedas.tamborTres.getPosicion());
+	}
+	
 	@Test
 	public void testPremio(){
 		Tragamonedas miTragamonedas = new Tragamonedas();
 		miTragamonedas.activar();
 		
 		while(miTragamonedas.entregaPremio() == false){
+			assertFalse(miTragamonedas.entregaPremio());
 			miTragamonedas.activar();
 		}
 		if(miTragamonedas.entregaPremio() == true){
-			System.out.println("Posicion ganar");
-			System.out.println("posicion tambor uno "+miTragamonedas.tamborUno.getPosicion());
-			System.out.println("posicion tambor dos "+miTragamonedas.tamborDos.getPosicion());
-			System.out.println("posicion tambor dos "+miTragamonedas.tamborTres.getPosicion());
+			assertTrue(miTragamonedas.entregaPremio());
 		}
 	}
+	@Test
+	public void seCreeUnNumeroAleatorioEntreUnoYDiez() {
+		RandoNumberGenerator newGenerator = new RandoNumberGenerator();
+		Integer numeroAleatorio = newGenerator.generate();
+		assertTrue(numeroAleatorio >=1 && numeroAleatorio <= 10);
+	}
+
 }
